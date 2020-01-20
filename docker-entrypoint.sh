ff96031e6dc31bcfbddebb0d529eadfe64236486
Vladimir Lysenko <x55aah@gmail.com>
2020-01-20 16:01:34 +0200

    update docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,6 +12,10 @@ python manage.py migrate
 #echo "Run startparser"
 #python manage.py startparser
 
+# Run hn posts app tests
+echo "Run testing"
+python manage.py test posts
+
 # Start server
 echo "Starting server"
 python manage.py runserver 0.0.0.0:8000
